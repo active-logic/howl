b2b7e32ad2171e4b6540672062120d33add6de4f
T.E.A de Souza <tea.desouza@gmail.com>
2020-08-29 02:16:39 +0800

    Win related fixes

---
+++ install.ps1
@@ -0,0 +1,12 @@
+# Assuming `howl install .`
+# First delegate to `howl publish .` with
+# config = release and no runtime arg
+# howl export src build/src
+pushd build
+# dotnet publish src -c Release --nologo
+# Above should build to
+# {π}/build/src/bin/Release/netcoreapp3.1/win-x64/publish"
+# But again, we can directly send to program files instead... maybe
+dotnet publish --nologo -o "C:/Program Files/Howl"
+mv -fo "C:\Program Files\Howl\src.exe" "C:\Program Files\Howl\howl.exe"
+popd  # exit build/
